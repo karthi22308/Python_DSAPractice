a028c2750854258ff0b9111cb7fad2d82646ddcc
1000070232 <karthi22308@gmail.com>
2024-09-15 19:58:21 +0530

    basics of python

--- main.py
+++ main.py
@@ -1,1 +1,75 @@
+
 print('helloworld')
+#comments
+print ("hello world")
+
+print ("hi "," hello")
+
+
+
+k=80
+ab = "hello dood"
+
+print(k,ab)
+
+type(ab)
+
+#to check the datatype of a variable
+print(type(k),type(ab))
+
+
+#location of variable-- pointer
+print(id(k))
+
+
+#none datatype
+
+k= None
+print(k)
+
+
+
+#getting input
+
+var = input("say something")
+
+print (var)
+
+#typecasting --- changing the memory location also ---
+var = int (var)
+print (type(var))
+
+
+
+## multitilne statement
+
+bc = 1+2+\
+    3
+bb = (1+2+
+      4)
+
+
+print(bc,bb)
+
+#output formatiing
+
+print("first value {} and here goes the secondd value{}".format(bc,bb))
+
+
+
+print("first value {1} and here goes the secondd value{0}".format(bc,bb)) ## we can change where to pass what
+
+
+
+print("testing {bc} hello".format())
+
+
+
+
+
+
+
+
+
+
+
